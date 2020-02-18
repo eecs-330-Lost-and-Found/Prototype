dc10403630e9e2b1f74f8cbab6fe2cee0255c095
Andrew Chung <drew.c3071@gmail.com>
2020-02-18 10:02:44 -0600

    add subheader and handling for date input

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ rewardInput.oninput = e => {
     rewardError.style.display = "none";
   }
 };
+
+const dateInput = document.querySelector("#date-input");
+const dateError = document.querySelector("#date-error");
+dateInput.oninput = e => {
+  const currVal = e.target.value;
+  const isDate = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/.test(
+    currVal
+  );
+  if (!isDate && currVal !== "") {
+    dateError.style.display = "flex";
+  } else {
+    dateError.style.display = "none";
+  }
+};
