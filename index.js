2c5e9b80568f466a772f131a67ba9b906ff429b8
Andrew Chung <drew.c3071@gmail.com>
2020-02-17 21:33:55 -0600

    change "money" to "reward"

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const moneyInput = document.querySelector("#money-input");
-const moneyError = document.querySelector("#money-error");
-moneyInput.oninput = e => {
-	const currVal = e.target.value;
-	const isPrice = /^[0-9]+(\.[0-9][0-9]?)?$/.test(currVal);
-	if (!isPrice) {
-		moneyError.style.display = "block";
-	}
-	else{
-		moneyError.style.display = "none";
-	}
-
+const rewardInput = document.querySelector("#reward-input");
+const rewardError = document.querySelector("#reward-error");
+rewardInput.oninput = e => {
+  const currVal = e.target.value;
+  const isPrice = /^[0-9]+(\.[0-9][0-9]?)?$/.test(currVal);
+  if (!isPrice) {
+    rewardError.style.display = "block";
+  } else {
+    rewardError.style.display = "none";
+  }
 };
