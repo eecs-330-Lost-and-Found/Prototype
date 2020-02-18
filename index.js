edf99631efd47fb592aeac2d9c43bb08162ff1aa
Andrew Chung <drew.c3071@gmail.com>
2020-02-18 00:21:21 -0600

    complete responsive styling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const rewardError = document.querySelector("#reward-error");
 rewardInput.oninput = e => {
   const currVal = e.target.value;
   const isPrice = /^[0-9]+(\.[0-9][0-9]?)?$/.test(currVal);
-  if (!isPrice) {
+  if (!isPrice && currVal !== "") {
     rewardError.style.display = "block";
   } else {
     rewardError.style.display = "none";
