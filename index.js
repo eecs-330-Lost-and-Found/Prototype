2da5b2f9b3f397c3fb13238d71fb3da73d56357c
joker00zss <55814816+joker00zss@users.noreply.github.com>
2020-02-25 01:59:08 -0600

    HW06

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
-const rewardInput = document.querySelector("#reward-input");
-const rewardError = document.querySelector("#reward-error");
-rewardInput.oninput = e => {
-  const currVal = e.target.value;
-  const isPrice = /^[0-9]+(\.[0-9][0-9]?)?$/.test(currVal);
-  if (!isPrice && currVal !== "") {
-    rewardError.style.display = "flex";
-  } else {
-    rewardError.style.display = "none";
-  }
-};
+const message1 = () => {
+    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm joker, I think I found your phone, please reply me in this board."
+}
 
-const dateInput = document.querySelector("#date-input");
-const dateError = document.querySelector("#date-error");
-dateInput.oninput = e => {
-  const currVal = e.target.value;
-  const isDate = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/.test(
-    currVal
-  );
-  if (!isDate && currVal !== "") {
-    dateError.style.display = "flex";
-  } else {
-    dateError.style.display = "none";
-  }
-};
+const message2 = () => {
+    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm kkkk, I think the wallet you find is mine, please contact me when you are free."
+}
 
-const uploader = document.querySelector("#file-upload");
-uploader.onchange = e => {
-  const imgViewer = document.querySelector(".image-viewer");
-  imgViewer.style.backgroundImage = `url(${URL.createObjectURL(
-    e.target.files[0]
-  )})`;
-};
+const message3 = () => {
+    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm david, could you sell the phone you found to me? I can give you more!"
+}
 
-const submitButton = document.querySelector("#submit");
-submitButton.onclick = e => {
-  const textFields = document.querySelectorAll(".form-input");
-  const radioChecked = document.querySelector("input[name='radius']:checked");
-  const textFilledOut = Array.from(textFields).every(
-    field => field.value || field.value === 0
-  );
-  const inputValidation =
-    rewardError.style.display === "none" && dateError.style.display === "none";
-  if (textFilledOut && radioChecked && inputValidation) {
-    alert("You have successfully filed a lost item!");
-  } else if (!textFilledOut || !radioChecked) {
-    alert("Please fill out all required fields.");
-  } else {
-    e.preventDefault();
-    alert("Please check the format of the reward and date lost.");
-  }
-};
+
+const submitButton = document.getElementById("submit");
+
+submitButton.onclick = (ev) => {
+    t = document.getElementById('track_template');
+
+    if (document.getElementById('messagearea').innerHTML == "") {
+        document.getElementById('replyarea').innerHTML = "please choose a message to reply."
+    }
+    else if (document.getElementById('comment').value == "")
+    {
+        document.getElementById('replyarea').innerHTML = "please leave a comment."
+    }
+    else if (document.getElementById('comment_author').value == "") {
+        document.getElementById('replyarea').innerHTML = "please leave your name."
+    }
+    else
+    {
+        document.getElementById('replyarea').innerHTML = document.getElementById('comment').value + "<br /> by  " + document.getElementById('comment_author').value;
+    }
+}
