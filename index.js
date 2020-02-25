ccb642df4419108d6c7d728a377287614f595b00
joker00zss <55814816+joker00zss@users.noreply.github.com>
2020-02-25 02:57:15 -0600

    p6

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+var reply1 = ""
+var reply2 = ""
+var reply3 = ""
+var message_1 = "Hi,<br /> I'm joker, I think I found your phone, please reply me in this board.<br />2020.1.20"
+var message_2 = "Hi,<br /> I'm kkkk, I think the wallet you find is mine, please contact me when you are free.<br />2020.2.20"
+var message_3 = "Hi,<br /> I'm david, could you sell the phone you found to me? I can give you more!<br />2020.1.23"
+
+
 const message1 = () => {
-    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm joker, I think I found your phone, please reply me in this board."
+    document.getElementById("messagearea").innerHTML = message_1
+    document.getElementById('replyarea').innerHTML = reply1
 }
 
 const message2 = () => {
-    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm kkkk, I think the wallet you find is mine, please contact me when you are free."
+    document.getElementById("messagearea").innerHTML = message_2
+    document.getElementById('replyarea').innerHTML = reply2
 }
 
 const message3 = () => {
-    document.getElementById("messagearea").innerHTML = "Hi,<br /> I'm david, could you sell the phone you found to me? I can give you more!"
+    document.getElementById("messagearea").innerHTML = message_3
+    document.getElementById('replyarea').innerHTML = reply3
 }
 
 
@@ -23,11 +34,39 @@ submitButton.onclick = (ev) => {
     {
         document.getElementById('replyarea').innerHTML = "please leave a comment."
     }
-    else if (document.getElementById('comment_author').value == "") {
+    else if (document.getElementById('comment_author').value == "" && !document.getElementById('anonymous').checked) {
         document.getElementById('replyarea').innerHTML = "please leave your name."
     }
     else
     {
-        document.getElementById('replyarea').innerHTML = document.getElementById('comment').value + "<br /> by  " + document.getElementById('comment_author').value;
+        if (document.getElementById('anonymous').checked) {
+            document.getElementById('replyarea').innerHTML = document.getElementById('comment').value + "<br /> by  anonymous";
+        }
+        else {
+            document.getElementById('replyarea').innerHTML = document.getElementById('comment').value + "<br /> by  " + document.getElementById('comment_author').value;
+        }
     }
+}
+
+const sort1 = () => {
+
+    if (document.getElementById('sort').value == "date") {
+        message_1 = "Hi,<br /> I'm joker, I think I found your phone, please reply me in this board.<br />2020.1.20" 
+        message_2 = "Hi,<br /> I'm david, could you sell the phone you found to me? I can give you more!<br />2020.1.23"
+        message_3 = "Hi,<br /> I'm kkkk, I think the wallet you find is mine, please contact me when you are free.<br />2020.2.20"
+        document.getElementById('message1').innerHTML = "Message1<br />from joker"
+        document.getElementById('message2').innerHTML = "Message1<br />from david"
+        document.getElementById('message3').innerHTML = "Message1<br />from kkkk"
+    }
+
+    else {
+        message_1 = "Hi,<br /> I'm david, could you sell the phone you found to me? I can give you more!<br />2020.1.23"
+        message_2 = "Hi,<br /> I'm joker, I think I found your phone, please reply me in this board.<br />2020.1.20"
+        message_3 = "Hi,<br /> I'm kkkk, I think the wallet you find is mine, please contact me when you are free.<br />2020.2.20"
+        document.getElementById('message1').innerHTML = "Message1<br />from david"
+        document.getElementById('message2').innerHTML = "Message1<br />from joker"
+        document.getElementById('message3').innerHTML = "Message1<br />from kkkk"
+
+    }
+
 }
